07c00cc190538e3f6003593cf2cd2746085543f6
mattiaslarssonjkpg <mattias_larsson_jkpg@hotmail.com>
2016-05-26 21:40:22 +0200

    Add files via upload

---
+++ Bluetooth_HC06.ino
@@ -0,0 +1,20 @@
+String command = ""; // Stores response of the HC-06 Bluetooth device
+
+void setup() {
+  Serial.begin(9600);
+  delay(10);
+  pinMode(13,OUTPUT);
+  delay(10);
+  digitalWrite(13, HIGH);
+}
+
+void loop() {
+    if (Serial.available()) {
+    while(Serial.available()) { // While there is more to be read, keep reading.
+      digitalWrite(13, LOW);
+      command += (char)Serial.read();
+    }
+    Serial.println(command);
+    command = ""; // No repeats
+  }
+}
